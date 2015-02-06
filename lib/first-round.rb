d020681070a3d3a33d891b4d2b88bf098ebc8783
Chris Ward <chriswarddev@gmail.com>
2015-02-06 14:02:42 +0000

    test passed and method works

--- lib/first-round.rb
+++ lib/first-round.rb
@@ -1,2 +1,12 @@
 # Write your code here.
-
+def to_wavecase(input_string)
+	output_string = ''
+	input_string.downcase.chars.each_with_index do |char, index|
+		if index.even?
+			output_string << char.upcase
+		else
+			output_string << char
+		end
+	end
+	output_string
+end
--- spec/first-round_spec.rb
+++ spec/first-round_spec.rb
@@ -1,8 +1,15 @@
 require('./lib/first-round')
 
-describe('') do
+describe('to_wavecase') do
 
-  it('') do
-    expect().to eq()
+  it('should return "A" when passed string "a"') do
+    expect(to_wavecase("a")).to eq("A")
   end
+  it 'should return "A" when passed string "A' do
+  	expect(to_wavecase("A")).to eq("A")
+  end
+  it 'should return "Ab" when passed string "ab"' do
+  	expect(to_wavecase("ab")).to eq("Ab")
+  end
+
 end
